d5cdb4246a115f357411f27af0c7eee41e67bde3
Sebastian Salazar Pulgar <sebanicosp@gmail.com>
2019-02-25 16:18:45 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,25 @@
 /// Puerto
 /// ====================
 process.env.PORT = process.env.PORT || 3000;
+
+/// ====================
+/// ENVIROMENT
+/// ====================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+/// ====================
+/// DATABASE
+/// ====================
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+
+} else {
+    urlDB = 'mongodb+srv://seba:1q2w3e@cafe-qvfbm.mongodb.net/test?retryWrites=true';
+}
+
+//urlDB = 'mongodb+srv://seba:1q2w3e@cafe-qvfbm.mongodb.net/test?retryWrites=true';
+
+
+process.env.URLDB = urlDB;
